c1de42ca608431c72584406618040ab9cec0e23e
Alexander Spiridonov <alex.spiridonov@castle.co>
2021-03-30 11:31:00 +0400

    useContent with alias

--- src/hooks/useContent.js
+++ src/hooks/useContent.js
@@ -1,5 +1,5 @@
 const useContent = (fileName, lang = 'ru') => (
-  require(`../content/${fileName}.${lang}.json`)
+  require(`@/content/${fileName}.${lang}.json`)
 );
 
 export default useContent;
@@ -10,7 +10,7 @@ import { useState, useEffect } from 'react';
 
 const useContent = (fileName, lang = 'ru') => {
   // eslint-disable-next-line global-require,import/no-dynamic-require
-  const pageContent = require(`../content/${fileName}.${lang}.json`);
+  const pageContent = require(`@/content/${fileName}.${lang}.json`);
 
   const [data, setData] = useState(
     (
